14296edf4f66821fdcfad0159b19494d7b82dd05
Deise Andrade <deise_andrade_paz@hotmail.com>
2025-06-13 15:05:24 -0300

    feat: Adicao do findByCpf e findByEmail no UsuarioRepository

--- src/main/java/com/tecnosfera/agendamedica/repository/UsuarioRepository.java
+++ src/main/java/com/tecnosfera/agendamedica/repository/UsuarioRepository.java
@@ -1,10 +1,17 @@
 package com.tecnosfera.agendamedica.repository;
 
 import com.tecnosfera.agendamedica.model.Usuario;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
+    Optional<Usuario> findByCpf(String cpf);
+
+    Optional<Usuario> findByEmail(String email);
 }
