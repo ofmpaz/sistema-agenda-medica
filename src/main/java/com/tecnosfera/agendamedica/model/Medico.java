7aec0061b9da47fc6abaa766d93998c35a2de0b7
Deise Andrade <deise_andrade_paz@hotmail.com>
2025-06-13 13:55:29 -0300

    refactor: correcao do nome da variavel crm e adicao nas validacao com spring

--- src/main/java/com/tecnosfera/agendamedica/model/Medico.java
+++ src/main/java/com/tecnosfera/agendamedica/model/Medico.java
@@ -3,13 +3,21 @@ package com.tecnosfera.agendamedica.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
 @Entity
 @Table(name = "medico")
 public class Medico extends Usuario {
 
     @Column(nullable = false)
-    private String CRM;
+    private String crm;
 
     @Column(nullable = false)
     private String especialidade;
--- src/main/java/com/tecnosfera/agendamedica/model/Paciente.java
+++ src/main/java/com/tecnosfera/agendamedica/model/Paciente.java
@@ -1,11 +1,27 @@
 package com.tecnosfera.agendamedica.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
 @Entity
 @Table(name = "paciente")
 public class Paciente extends Usuario {
 
+
+    @Min(0)
+    @Max(120)
     @Column(nullable = false)
+    @NotNull
     private int idade;
 }
