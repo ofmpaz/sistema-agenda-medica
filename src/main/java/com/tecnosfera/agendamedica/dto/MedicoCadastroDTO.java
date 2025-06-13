3cc740f3c8d7667790dab34c5a05b45eeab8279d
Deise Andrade <deise_andrade_paz@hotmail.com>
2025-06-13 13:53:12 -0300

    feat: Criacao do record dto MedicoCadastroDTO e PacienteCadastroDTO

---
+++ src/main/java/com/tecnosfera/agendamedica/dto/MedicoCadastroDTO.java
@@ -0,0 +1,30 @@
+package com.tecnosfera.agendamedica.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record MedicoCadastroDTO(
+
+        @NotBlank
+        String nomeCompleto,
+
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{11}", message = "CPF deve conter 11 dígitos")
+        String cpf,
+
+        @NotBlank
+        String crm,
+
+        @NotBlank
+        String especialidade,
+
+        @NotBlank
+        String telefone
+) {
+
+}
---
+++ src/main/java/com/tecnosfera/agendamedica/dto/PacienteCadastroDTO.java
@@ -0,0 +1,24 @@
+package com.tecnosfera.agendamedica.dto;
+
+import jakarta.validation.constraints.*;
+
+public record PacienteCadastroDTO(
+
+        @NotBlank
+        String nomeCompleto,
+
+        @Min(0)
+        int idade,
+
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{11}", message = "CPF deve conter 11 dígitos")
+        String cpf,
+
+        @NotBlank
+        String telefone
+) {
+}
