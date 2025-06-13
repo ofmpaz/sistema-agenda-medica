b3a7e5358fc4bb892fe8bea5fd0719963c599e81
Deise Andrade <deise_andrade_paz@hotmail.com>
2025-06-13 13:56:21 -0300

    refactor: transformacao da classe usuario em abstract

--- src/main/java/com/tecnosfera/agendamedica/model/Usuario.java
+++ src/main/java/com/tecnosfera/agendamedica/model/Usuario.java
@@ -1,22 +1,44 @@
 package com.tecnosfera.agendamedica.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
 @Entity
 @Table(name= "usuario")
 @Inheritance(strategy = InheritanceType.JOINED)
-public class Usuario {
+public abstract class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
+    @Size(min = 3)
     @Column(nullable = false)
     private String nomeCompleto;
 
+    @NotBlank
+    @Pattern(regexp = "\\d{11}", message = "CPF deve conter 11 dígitos")
     @Column(nullable = false)
-    private String CPF;
+    private String cpf;
 
+    @NotBlank
+    @Email
     @Column(nullable = false)
     private String email;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String telefone;
 }
