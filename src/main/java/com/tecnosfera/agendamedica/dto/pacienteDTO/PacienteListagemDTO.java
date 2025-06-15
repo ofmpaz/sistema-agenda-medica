7185c5e1913adf45b8007191ee3ef45b52ca1704
Deise Andrade <deise_andrade_paz@hotmail.com>
2025-06-15 13:15:05 -0300

    implementaçao do dto PacienteListagemDTO, responsável por listar todos os pacientes cadastrados

--- src/main/java/com/tecnosfera/agendamedica/dto/pacienteDTO/PacienteListagemDTO.java
+++ src/main/java/com/tecnosfera/agendamedica/dto/pacienteDTO/PacienteListagemDTO.java
@@ -1,4 +1,19 @@
 package com.tecnosfera.agendamedica.dto.pacienteDTO;
 
-public enum PacienteListagemDTO {
+import com.tecnosfera.agendamedica.model.Paciente;
+
+public record PacienteListagemDTO(
+        Long id,
+        String nomeCompleto,
+        String email,
+        String telefone
+) {
+
+    public PacienteListagemDTO(Paciente paciente) {
+        this(paciente.getId(),
+                paciente.getNomeCompleto(),
+                paciente.getEmail(),
+                paciente.getTelefone());
+    }
 }
+
